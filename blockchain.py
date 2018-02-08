8881a5e75a8229778c858928ea5f2a391ec7a067
talentedev <webdev124@outlook.com>
2018-02-08 14:36:00 +0100

    initial commit

---
+++ blockchain.py
@@ -0,0 +1,21 @@
+# Module 1 - Create a Blockchain
+
+# To be installed:
+# Flask==0.12.2: pip install Flask==0.12.2
+# Postman HTTP Client: https://www.getpostman.com/
+
+# Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 - Building a Blockchain
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+
+# Part 2 - Mining our Blockchain
